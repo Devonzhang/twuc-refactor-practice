e0cef658db40e4ed3bddefd01a53cc6cc8bc6b41
devonzhang <devonzhang1024@gmail.com>
2021-08-08 16:29:43 +0800

    finished 6

--- 6/src/main/java/com/twu/refactoring/Customer.java
+++ 6/src/main/java/com/twu/refactoring/Customer.java
@@ -1,11 +1,11 @@
 package com.twu.refactoring;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.stream.Collectors;
 
 public class Customer {
 
-    private static final String STATEMENT_HEADER = "Rental Record for ";
+    private static final String RENTAL_RECORD_FOR = "Rental Record for ";
     private static final String AMOUNT_OWED_IS = "Amount owed is ";
     private static final String YOU_EARNED = "\nYou earned ";
     private static final String FREQUENT_RENTER_POINTS = " frequent renter points";
@@ -27,11 +27,8 @@ public class Customer {
     public String statement() {
         double totalAmount = rentalList.stream().mapToDouble(Rental::getThisAmount).sum();
         int frequentRenterPoints = rentalList.stream().mapToInt(Rental::getFrequentRenterPoints).sum();
-        String result = STATEMENT_HEADER + getName() + "\n";
-        result += rentalList.toString().replaceAll("\\[", "")
-                .replaceAll("]", "")
-                .replaceAll(",", "")
-                .replaceAll(" \t", "\t");
+        String result = RENTAL_RECORD_FOR + getName() + "\n";
+        result += rentalList.stream().map(Rental::toString).collect(Collectors.joining());
         result += AMOUNT_OWED_IS + totalAmount;
         result += YOU_EARNED + frequentRenterPoints + FREQUENT_RENTER_POINTS;
         return result;
--- 6/src/main/java/com/twu/refactoring/Rental.java
+++ 6/src/main/java/com/twu/refactoring/Rental.java
@@ -2,6 +2,15 @@ package com.twu.refactoring;
 
 public class Rental {
 
+    private static final int REGULAR_RENTAL_AMOUNT = 2;
+    private static final int REGULAR_RENTAL_TIME = 2;
+    private static final double RENTAL_EXPIRE_CHARGE_RATE = 1.5;
+    private static final int NEW_RELEASE_RENTAL_AMOUNT = 3;
+    private static final double CHILDREN_RENTAL_AMOUNT = 1.5;
+    private static final int CHILDREN_RENTAL_TIME = 3;
+    private static final int NEW_RELEASE_FREQUENT_RENTAL_TIME = 1;
+    private static final int NEW_RELEASE_FREQUENT_POINTS_MORE_THAN_2_DAYS = 2;
+    private static final int NEW_RELEASE_FREQUENT_POINTS = 1;
     private Movie movie;
 
     private int daysRented;
@@ -15,17 +24,17 @@ public class Rental {
         double thisAmount = 0;
         switch (movie.getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2)
-                    thisAmount += (daysRented - 2) * 1.5;
+                thisAmount += REGULAR_RENTAL_AMOUNT;
+                if (daysRented > REGULAR_RENTAL_TIME)
+                    thisAmount += (daysRented - REGULAR_RENTAL_TIME) * RENTAL_EXPIRE_CHARGE_RATE;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += daysRented * 3;
+                thisAmount += daysRented * NEW_RELEASE_RENTAL_AMOUNT;
                 break;
             case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3)
-                    thisAmount += (daysRented - 3) * 1.5;
+                thisAmount += CHILDREN_RENTAL_AMOUNT;
+                if (daysRented > CHILDREN_RENTAL_TIME)
+                    thisAmount += (daysRented - CHILDREN_RENTAL_TIME) * RENTAL_EXPIRE_CHARGE_RATE;
                 break;
 
         }
@@ -39,7 +48,7 @@ public class Rental {
     }
 
     public int getFrequentRenterPoints() {
-        return movie.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1 ? 2 : 1;
+        return movie.getPriceCode() == Movie.NEW_RELEASE && daysRented > NEW_RELEASE_FREQUENT_RENTAL_TIME ? NEW_RELEASE_FREQUENT_POINTS_MORE_THAN_2_DAYS : NEW_RELEASE_FREQUENT_POINTS;
     }
 
 }
